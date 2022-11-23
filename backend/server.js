1baa14526084b8681cef8af156eed6833984e7fa
osamazaidiah <osama.zaidiah@gmail.com>
2022-11-23 09:04:32 +0300

    Express server running

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Server is started... and ready...");
+import express from "express";
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(express.json());
+
+app.get("/ping", (req, res) => {
+  console.log("Received a ping... responding with a pong...");
+  res.send("PONG back at you... 😊");
+});
+
+app.listen(PORT, () =>
+  console.log(`MERN server is listening on port ${PORT}...`)
+);
